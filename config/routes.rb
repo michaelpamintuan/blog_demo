4183d1a3d52cd5dd0c835541ece2051527fd36a4
michaelpamintuan <michael@paralejasconsulting.com>
2022-12-14 12:26:10 +0800

    Add view count

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
     sessions: 'users/sessions',
     registrations: 'users/registrations'
   }
+get '/u/:id', to: 'users#profile', as: 'user'
+
   resources :posts
   get 'about', to: 'pages#about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
