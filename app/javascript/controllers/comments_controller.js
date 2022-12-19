bf23ab970c1794e6b7c50757a593b5b80229f6f3
michaelpamintuan <michael@paralejasconsulting.com>
2022-12-19 17:48:49 +0800

    New blog_post

--- app/javascript/controllers/comments_controller.js
+++ app/javascript/controllers/comments_controller.js
@@ -7,5 +7,12 @@ export default class extends Controller {
     console.log("I clicked the edit button.");
     event.preventDefault();
     event.stopPropagation();
+    const formID = event.params["form"];
+    const commentBodyID = event.params["body"];
+    const form = document.getElementById(formID);
+    form.classList.toggle("d-none");
+    form.classList.toggle("mt-5");
+    const commentBody = document.getElementById(commentBodyID);
+    commentBody.classList.toggle("d-none");
   }
 }
