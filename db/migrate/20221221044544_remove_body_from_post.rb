788e2a1fef0f498395a5ad6a06eb74b89d976fa3
michaelpamintuan <michael@paralejasconsulting.com>
2022-12-21 17:32:11 +0800

    Creating an Admin Dashboard

---
+++ db/migrate/20221221044544_remove_body_from_post.rb
@@ -0,0 +1,4 @@
+class RemoveBodyFromPost < ActiveRecord::Migration[7.0]
+  def change
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,26 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-User.create(email: "kix@gmail.com", name: "Kix", password: "password", password_confirmation: "password")
-User.create(email: "det@gmail.com", name: "Dette", password: "password", password_confirmation: "password")
-
+User.create(email: 'kix@gmail.com',
+            password: 'password',
+            password_confirmation: 'password',
+            name: 'Kix',
+            role: User.roles[:admin])
+User.create(email: 'det@gmail.com',
+            password: 'password',
+            password_confirmation: 'password',
+            name: 'Dette')
+            
 10.times do |x|
-    Post.create(title: "Title #{x}", body: "Body #{x} Words go here Idk", user_id: User.first.id)
-end
+  post = Post.create(title: "Title #{x}",
+                    body: "Body #{x} Words go here Idk",
+                    user_id: User.first.id)
+
+    5.times do |y|
+        Comment.create(body: "Comment #{y}",
+                       user_id: User.second.id,
+                       post_id: post.id)
+    end
+end                
+                      
+                    
